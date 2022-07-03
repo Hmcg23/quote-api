6bc352961c7135e61ff3103e0cb3422197c22bc6
H_mcg <82006254+Hmcg23@users.noreply.github.com>
2022-07-03 00:43:37 -0700

    add two get requets for random quotes

--- server.js
+++ server.js
@@ -8,3 +8,29 @@ const PORT = process.env.PORT || 4001;
 
 app.use(express.static('public'));
 
+app.listen(PORT, () => {
+    console.log('Listening on port 4001!');
+})
+
+app.get('/api/quotes/random', (req, res, next) => {
+    res.send({
+        quote: getRandomElement(quotes)
+    })
+})
+
+app.get('/api/quotes', (req, res, next) => {
+    const person = req.query.person;
+    if (person) {
+        const personQuotes = quotes.filter(quote => quote.person === person);
+        console.log(personQuotes);
+        res.send({
+            quotes: personQuotes
+        })
+        
+    } else {
+        res.send({
+            quotes: []
+        })
+    }
+})
+
